865e2b497b58ea1fe0f72f2181c9c07247e1c9ba
zhilinIPD19 <zhilin.lin@yahoo.com>
2020-01-17 19:30:50 -0500

    create structure

--- CreateStructure/Program.cs
+++ CreateStructure/Program.cs
@@ -6,10 +6,50 @@ using System.Threading.Tasks;
 
 namespace CreateStructure
 {
+    public enum Genders { Male, Female };
+    struct Person
+    {
+        public string firstName;
+        public string lastName;
+        public int age;
+        public Genders gender;
+
+        public Person(string _firstName, string _lastName, int _age, Genders _gender)
+        {
+            firstName = _firstName;
+            lastName = _lastName;
+            age = _age;
+            gender = _gender;     
+        }
+        public override string ToString()
+        {
+            return firstName + " " +  lastName + " "+ gender+" " + age;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            Person p = new Person("Tony", "Allen", 32, Genders.Female);
+            Console.WriteLine(p);
+            var k = Console.ReadKey();
+
+            switch (k.KeyChar)
+            {
+                case 'a':
+                case 'b':
+                    break;
+                case 'c':
+                    Console.WriteLine("It's a C.");
+                    break;
+                default:
+                    break;
+
+
+
+            }
+            Console.ReadKey();
+
         }
     }
 }
